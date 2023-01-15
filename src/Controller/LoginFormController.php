1b23c8665875705ec4834cb5ce3dfe466dd3b9b5
Eduardo Teixeira <dunirvana@gmail.com>
2023-01-15 16:52:59 -0300

    Criado o processo de logout

--- src/Controller/LoginFormController.php
+++ src/Controller/LoginFormController.php
@@ -8,11 +8,11 @@ class LoginFormController implements Controller
 {
     public function processaRequisicao(): void
     {
-      if ($_SESSION['logado'] === true) {
+      if (array_key_exists('logado', $_SESSION) && $_SESSION['logado'] === true) {
         header ('Location: /');
         return;
       }      
-      
+
       require_once __DIR__ . '/../../views/login-form.php';
     }
 }
